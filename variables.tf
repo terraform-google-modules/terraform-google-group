11cfd134ce088247ef5b90ce0d24b0a8a0d6a94a
Awais Malik <awmalik@google.com>
2023-05-08 14:15:11 -0700

    fixed lint issues

--- variables.tf
+++ variables.tf
@@ -15,45 +15,54 @@
  */
 
 variable "id" {
+  type = string
   description = "ID of the group. For Google-managed entities, the ID must be the email address the group"
 }
 
 variable "display_name" {
+  type = string
   description = "Display name of the group"
   default     = ""
 }
 
 variable "description" {
+  type = string
   description = "Description of the group"
   default     = ""
 }
 
 variable "domain" {
+  type = string
   description = "Domain of the organization to create the group in. One of domain or customer_id must be specified"
   default     = ""
 }
 
 variable "customer_id" {
+  type = string
   description = "Customer ID of the organization to create the group in. One of domain or customer_id must be specified"
   default     = ""
 }
 
 variable "owners" {
+  type = list(string)
   description = "Owners of the group. Each entry is the ID of an entity. For Google-managed entities, the ID must be the email address of an existing group, user or service account"
   default     = []
 }
 
 variable "managers" {
+  type = list(string)
   description = "Managers of the group. Each entry is the ID of an entity. For Google-managed entities, the ID must be the email address of an existing group, user or service account"
   default     = []
 }
 
 variable "members" {
+  type = list(string)
   description = "Members of the group. Each entry is the ID of an entity. For Google-managed entities, the ID must be the email address of an existing group, user or service account"
   default     = []
 }
 
 variable "initial_group_config" {
+  type = string
   description = "The initial configuration options for creating a Group. See the API reference for possible values. Possible values are INITIAL_GROUP_CONFIG_UNSPECIFIED, WITH_INITIAL_OWNER, and EMPTY."
   default     = "EMPTY"
 }
